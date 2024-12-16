5f7b3f2f965705f495bfdcf9165b54fc9fde7883
panameraxx <90213411+panameraxx@users.noreply.github.com>
2024-12-16 22:05:46 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Output(ArrayList kol)
+        {
+            foreach (var e in kol)
+            {
+                Console.WriteLine(e);
+            }
+        }
+        static void Main(string[] args)
+        {
+            ArrayList line = new ArrayList();
+            line.Add(3);
+            line.Add("Строковое");
+            line.Add(true);
+            line.Add(2.76);
+            line.Add(-23);
+            line.Add(-4.23);
+            Output(line);
+            Console.WriteLine();
+            int kolvo = line.Count;
+            Console.WriteLine($"Количесво веществ: {kolvo}");
+            Console.WriteLine();
+            line.Insert(2, "чтото новое");
+            Output(line);
+            Console.WriteLine();
+            line.RemoveAt(0);
+            Output(line);
+            Console.WriteLine();
+            line.Reverse();
+            Output(line);
+            Console.WriteLine();
+            string[] massivArray = { "Тут1", "Тут2", "Тут3" };
+            line.AddRange(massivArray);
+            Output(line);
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/ConsoleApp1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Atlas Playbook v0.4.1")]
+[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyCopyright("Copyright © Atlas Playbook v0.4.1 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("01be2aa0-b7bf-4f7b-9bf6-8d4707c8520d")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
